3a2245c0a5435ab874e4259e3d2d14c993f36172
StephaneCash <kikonistephane@gmail.com>
2022-06-03 14:13:43 +0100

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Grid, Paper, Avatar, TextField } from "@material-ui/core"
+import LockIcon from '@mui/icons-material/Lock';
+import '../assets/Login.css';
 
 function Login() {
+
+  const paperStyle = { padding: 20, height: '70vh', width: 340, margin: '20px auto' }
+  const backgroundColorAvatar = { background: 'blue' };
+  const styleTextField = { height: '11vh' }
+
   return (
-    <div>Login</div>
+    <div className="login">
+      <Grid>
+        <Paper elevation={10} style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={backgroundColorAvatar}> <LockIcon /></Avatar>
+            <h2 className='mt-3'>S'identifier</h2>
+          </Grid>
+
+          <TextField style={styleTextField} placeholder="Nom d'utilisateur ou email" label="Username" fullWidth />
+
+        </Paper>
+      </Grid>
+    </div>
   )
 }
 
