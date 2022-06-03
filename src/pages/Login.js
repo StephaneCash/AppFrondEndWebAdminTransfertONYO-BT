63077ddf9ec2b6c4e687ca481f8f297e78513cbc
StephaneCash <kikonistephane@gmail.com>
2022-06-03 16:07:48 +0100

    loginFinishForm

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Grid, Paper, Avatar, TextField } from "@material-ui/core"
+import { Grid, Paper, Avatar, TextField, FormControlLabel, Button, Typography, Link } from "@material-ui/core"
 import LockIcon from '@mui/icons-material/Lock';
+import Checkbox from '@mui/material/Checkbox';
 import '../assets/Login.css';
 
 function Login() {
 
   const paperStyle = { padding: 20, height: '70vh', width: 340, margin: '20px auto' }
   const backgroundColorAvatar = { background: 'blue' };
-  const styleTextField = { height: '11vh' }
+  const styleTextField = { marginBottom: '10px' }
+  const ButtonStyle = { margin: '8px 0' }
+
+  const formControlLabel = {
+    marginLeft: 0,
+    marginBottom: '15px'
+  }
 
   return (
     <div className="login">
@@ -18,8 +25,36 @@ function Login() {
             <h2 className='mt-3'>S'identifier</h2>
           </Grid>
 
-          <TextField style={styleTextField} placeholder="Nom d'utilisateur ou email" label="Username" fullWidth />
+          <TextField placeholder="Nom d'utilisateur ou email"
+            label="Username" fullWidth required
+            className='mt-2'
+          />
+          <TextField style={styleTextField} placeholder="Mot de passe"
+            label="Mot de passe" type="password" fullWidth required />
 
+          <FormControlLabel
+            style={formControlLabel}
+            control={
+              <Checkbox
+                name="checkedB"
+                color="primary"
+              />
+            }
+            label="Se souvenir de moi"
+          />
+
+          <Button type="submit" variant="contained" style={ButtonStyle} color='primary' fullWidth>Se connecter</Button>
+          <Typography>
+            <Link href="#">
+              Mot de passe oublié ?
+            </Link>
+          </Typography>
+
+          <Typography> Avez-vous un compte ?
+            <Link href="#">
+              S'inscrire
+            </Link>
+          </Typography>
         </Paper>
       </Grid>
     </div>
