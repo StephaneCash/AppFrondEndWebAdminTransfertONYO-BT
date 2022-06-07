5466267d9130ce00b8901b77a6452e09dc6cabba
StephaneCash <kikonistephane@gmail.com>
2022-06-07 16:23:33 +0100

    rebours Compte

--- src/dialogs/AddTransaction.js
+++ src/dialogs/AddTransaction.js
@@ -73,10 +73,10 @@ function AddTransaction() {
         const min = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distanceBase % (1000 * 60)) / (1000))
 
-        dataRebours.jours = days
-        dataRebours.heures = heures
-        dataRebours.min = min
-        dataRebours.seconds = seconds
+        dataRebours.jours = days;
+        dataRebours.heures = heures;
+        dataRebours.min = min;
+        dataRebours.seconds = seconds;
     }
 
 
