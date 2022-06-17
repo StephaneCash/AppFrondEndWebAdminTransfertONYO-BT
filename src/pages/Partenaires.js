0ce370d2a381b44c8078f5acc303307c4a7b75fc
StephaneCash <kikonistephane@gmail.com>
2022-06-17 16:09:51 +0100

    add

--- src/pages/Partenaires.js
+++ src/pages/Partenaires.js
@@ -28,8 +28,6 @@ function Partenaires() {
 
     return (
         <div>
-
-
             <div className='col-12'>
                 <div className="d-flex">
                     <div className="col-2">
